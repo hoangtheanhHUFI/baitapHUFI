bdbd124de259001725804e5c018167642a925759
The Anh <2001202008@hufi.edu.vn>
2021-08-18 15:51:43 +0700

    changed

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,15 @@
 #include <iostream.h>
 using namespace std; 
+struct SinhVien {
+	char name[20];
+	int age, mssv;
+}; typedef SinhVien sv;
+struct linkedlist {
+	sv data;
+	linkedlist* next;
+}; typedef linkedlist node;
+
+
 int main()
 {
 	cout << "Hello word" << endl;
